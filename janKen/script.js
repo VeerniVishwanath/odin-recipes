c0470e00a0fec4e6b09f9e31af97806643be53a7
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2024-01-30 15:40:24 +0530

    Add Intro and Rules

--- janKen/script.js
+++ janKen/script.js
@@ -6,3 +6,18 @@ if (!alertShown) {
 
   sessionStorage.setItem("alertShown", true);
 }
+
+//Intro
+console.log(
+  "Konichiwa \n\nLet's play JanKen -The Japanese version of Rock Paper Scissors \n\n"
+);
+
+//Rules
+console.groupCollapsed("Rules");
+console.log(`1. Rules are same as the English version.
+2. Here,  Guu = Rock 
+          Paa = Paper
+          Choki = Scissors
+3. You'll play a five round game against the computer. 
+4. Refresh the page to reset the game.`);
+console.groupEnd();
